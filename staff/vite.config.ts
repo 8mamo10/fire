c0c561dcae1d940406ed5a18f62618f1871b4c95
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-02-12 19:02:39 +0700

    Adjust vite config

--- staff/vite.config.ts
+++ staff/vite.config.ts
@@ -1,8 +1,24 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import pages from "@hono/vite-build/cloudflare-pages";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
+import client from "honox/vite/client";
 
-export default defineConfig({
-  plugins: [honox({ devServer: { adapter } }), build()]
-})
+export default defineConfig(({ mode }) => {
+  if (mode === "client") {
+    return {
+      plugins: [client()],
+    };
+  } else {
+    return {
+      plugins: [
+        honox({
+          devServer: {
+            adapter,
+          },
+        }),
+        pages(),
+      ],
+    };
+  }
+});
