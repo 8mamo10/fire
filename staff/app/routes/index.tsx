dea5b052831bf33a0373ed4cd70418bc625b3e16
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2025-02-15 23:56:57 +0700

    Fix index page

--- staff/app/routes/index.tsx
+++ staff/app/routes/index.tsx
@@ -1,18 +1,29 @@
 import { css } from 'hono/css'
 import { createRoute } from 'honox/factory'
-import Counter from '../islands/counter'
+import type { FC } from 'hono/jsx'
+import type { Employee } from '../db'
+import { findAllEmployees, } from '../db'
 
 const className = css`
   font-family: sans-serif;
 `
 
-export default createRoute((c) => {
-  const name = c.req.query('name') ?? 'Hono'
+const EmployeeCard: FC<{ employee: Employee }> = ({ employee }) => {
+  const { employee_id, name, image_url, department_name, location_name } = employee;
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md">
+      <a href={`/ employee / ${employee_id}`}>
+        <img className="bg-indigo-600 p-4 rounded-t-lg" src={image_url} alt={name} />
+      </a>
+    </div>
+  );
+};
+
+export default createRoute(async (c) => {
+  const employees = await findAllEmployees(c.env.DB)
   return c.render(
     <div class={className}>
-      <h1>Hello, {name}!</h1>
-      <Counter />
+      <h1>Hello, World!!</h1>
     </div>,
-    { title: name }
   )
 })
